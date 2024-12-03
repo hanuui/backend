0dc9184a33fd878ee19347e8e71771857477d89e
user <ynzxc123@gmail.com>
2024-12-03 18:33:44 +0900

    hhhhhh

--- hkhk/backend/app.py
+++ hkhk/backend/app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 CORS(app)  # CORS 설정
 
 # CSV 파일 경로
-CSV_FILE_PATH = "./data/program.csv"
+CSV_FILE_PATH = "./data/program2.csv"
 
 # 데이터를 로드하는 함수
-def load_filtered_program_data(region=None, time=None, days=None, page=1, limit=20):
+def load_filtered_program_data(region=None, time=None, days=None, target=None, page=1, limit=20):
     try:
         # CSV 데이터를 pandas로 읽기
         df = pd.read_csv(CSV_FILE_PATH)
@@ -24,6 +24,11 @@ def load_filtered_program_data(region=None, time=None, days=None, page=1, limit=
         if days:
             for day in days:
                 df = df[df[day] == True]
+        
+        if target:
+            # target 값이 해당하는 열에서 True인 데이터만 선택
+            if target in ["child", "teen", "adult", "senior", "disorder"]:
+                df = df[df[target] == True]
 
         # 페이지네이션 처리
         start = (page - 1) * limit
@@ -43,11 +48,12 @@ def get_programs():
     region = request.args.get('region', None)
     time = request.args.get('time', None)
     days = request.args.getlist('days')  # 요일은 리스트 형태로 받음
+    target = request.args.get('target', None)  # 단일 값으로 받음
     page = int(request.args.get('page', 1))
     limit = int(request.args.get('limit', 20))
     
     # 데이터를 로드하고 필터링
-    data, total_count = load_filtered_program_data(region, time, days, page, limit)
+    data, total_count = load_filtered_program_data(region, time, days, target, page, limit)
     
     # 응답 데이터 형식
     response = {
